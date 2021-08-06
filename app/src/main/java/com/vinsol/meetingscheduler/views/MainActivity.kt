f5aebad54ed519d8436b6d04748ac5d673146f8d
shubham <shubhamsinghshubham777@gmail.com>
2021-08-06 11:29:26 +0530

    Added Splash Screen

--- app/src/main/java/com/vinsol/meetingscheduler/views/MainActivity.kt
+++ app/src/main/java/com/vinsol/meetingscheduler/views/MainActivity.kt
@@ -1,24 +1,22 @@
 package com.vinsol.meetingscheduler.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.viewbinding.library.activity.viewBinding
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.vinsol.meetingscheduler.R
-import com.vinsol.meetingscheduler.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by viewBinding()
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MeetingScheduler)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
